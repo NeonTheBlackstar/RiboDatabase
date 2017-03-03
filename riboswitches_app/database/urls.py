28a1c47c0f9f0d6b8dd2d2bfb26ee01a5bc1bb81
sSwiergosz <szymon.swiergosz@gmail.com>
2017-03-03 14:13:14 +0100

    Browsing by ligand and organism added

--- riboswitches_app/database/urls.py
+++ riboswitches_app/database/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'database'
 
 urlpatterns = [
-	url(r'^index/$', views.index, name="index"),
-    url(r'^browser/$', views.browser, name='browser'),
-    url(r'^ligand/$', views.detail, name="detail"),
+	url(r'^$', views.index, name="index"),
+    url(r'^ligand_browser/$', views.ligand_browser, name='ligand_browser'),
+    url(r'^ligand_browser/(?P<ligand_name>[A-Za-z]+)/$', views.ligand_detail, name="ligand_detail"),
+    url(r'^organism_browser/$', views.organism_browser, name='organism_browser'),
+    url(r'^organism_browser/(?P<organism_name>([-\w\s.()])+)/$', views.organism_detail, name="organism_detail"), ##TODO REGEX!!!
+    url(r'^searcher/', views.searcher, name='searcher'),
 ]
--- riboswitches_app/database/views.py
+++ riboswitches_app/database/views.py
@@ -1,25 +1,52 @@
 from django.shortcuts import render
 
-from .models import Ligand, RiboFamily, Record
+from .models import Gene, Organism, Ligand, RiboFamily, Record
 
 def index(request):
 
 	return render(request, 'database/index.html')
 
-def browser(request):
+def searcher(request):
+
+	return render(request, 'database/searcher.html')
+
+def ligand_browser(request):
 
 	ligand_list = Ligand.objects.all()
 	context = {
 		'ligand_list': ligand_list,
 	}
 
-	return render(request, 'database/browser.html', context)
+	return render(request, 'database/ligand_browser.html', context)
 
-def detail(request, ligand_name):
+def ligand_detail(request, ligand_name):
 
-	riboswitch_ligand = Ligand.objects.get(name = ligand_name)
+	riboswitch_ligand = Ligand.objects.filter(name = ligand_name)
+	riboswitch_family = RiboFamily.objects.filter(ligands = riboswitch_ligand)
+	riboswitch_record = Record.objects.filter(family = riboswitch_family)
 	context = {
 		'riboswitch_ligand': riboswitch_ligand,
+		'riboswitch_record': riboswitch_record,
 	}
 
-	return render(request, 'database/detail.html', context)
+	return render(request, 'database/ligand_detail.html', context)
+
+def organism_browser(request):
+
+	organism_list = Organism.objects.all()
+	context = {
+		'organism_list': organism_list,
+	}
+	
+	return render(request, 'database/organism_browser.html', context)
+
+def organism_detail(request, organism_name):
+
+	organism = Organism.objects.filter(scientific_name = organism_name)
+	gene = Gene.objects.filter(organism = organism)
+	riboswitch_record = Record.objects.filter(gene = gene)
+	context = {
+		'riboswitch_record': riboswitch_record,
+	}
+
+	return render(request, 'database/organism_detail.html', context)
